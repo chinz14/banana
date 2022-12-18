f31ec2b4766af10c9586a261f14e9936654c76ba
chinz-artt <77480676+chinz-artt@users.noreply.github.com>
2022-12-18 19:16:55 +0530

    day2:update

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 var translate=document.querySelector("#click");
 
 var txtinput = document.querySelector("#txt-input");
-console.log("txtinput");
-click.addEventListener("click" , function clickHandler() {
-    console.log("click");
-    console.log("input",txtinput.value);
-});
+var outputdiv = document.querySelector("#output");
+
+serverurl="		https://api.funtranslations.com/translate/braille.json";
+
+
+function getTranslationurl(text){
+    return serverurl + "?" +"text=" + text
+ }
+
+ function errorhandler(error){
+    console.log("Error: " + error);
+    alert("somethin went wrong  ");
+ }
+
+ function clickHandler(){
+    var inputtxt=txtinput.value;
+
+fetch(getTranslationurl(inputtxt))
+.then(response =>response.json())
+.then(json => {
+    var translatedtext =json.contents.translated;
+    outputdiv.innerText = translatedtext;
+})
+
+ }
 
 
 translate.addEventListener("click",clickHandler);
